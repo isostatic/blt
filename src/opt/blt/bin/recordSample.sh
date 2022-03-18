8c8edd9125b537f6935c0bdbbfca6682fd0820cc
Paul Weaver <Paul.Weaver@bbc.co.uk>
2022-03-18 14:19:47 +0000

    Fix JPEG creation for record sample

--- src/opt/blt/bin/recordSample.sh
+++ src/opt/blt/bin/recordSample.sh
@@ -27,10 +27,10 @@ sudo systemctl start blt-read.service
 "
 [1]showwaves=split_channels=1:mode=line:s=1920x200[vol];
 [0][vol]overlay=10:main_h-overlay_h-10
-" $TMPDIR/$SUBDIR/%04d.jpg
+"
 
 
-/opt/blt/bin/ffmpeg -f rawvideo -vcodec rawvideo -pix_fmt uyvy422 -s 1920x1080 -r 25 -i $TMPDIR/$NAME.yuv -f s16le -acodec pcm_s16le -ar 48000 -ac 2 -i $TMPDIR/$NAME.pcm -y -vcodec libx264 \
+/opt/blt/bin/ffmpeg -f rawvideo -vcodec rawvideo -pix_fmt uyvy422 -s 1920x1080 -r 25 -i $TMPDIR/$NAME.yuv -f s16le -acodec pcm_s16le -ar 48000 -ac 2 -i $TMPDIR/$NAME.pcm -y -vcodec mjpeg \
 -filter_complex \
 "
 [1]showwaves=split_channels=1:mode=line:s=1920x200[vol];
